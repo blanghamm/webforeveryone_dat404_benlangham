b662651f3ed26d7689db1b04be8beae4e0d98e0c
blanghamm <33899504+blanghamm@users.noreply.github.com>
2018-03-26 16:41:48 +0100

    update

---
+++ www/js/navbar.js
@@ -0,0 +1,13 @@
+function openNav() {
+    document.getElementById("mySidenav").style.width = "200px";
+    document.getElementById("mySidenav").style.visibility = "visible";
+    document.getElementById("navthing").style.visibility = "hidden";
+
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("mySidenav").style.visibility = "hidden";
+    document.getElementById("navthing").style.visibility = "visible";
+
+}
